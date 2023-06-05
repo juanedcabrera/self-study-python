d7cb2d3a0fad49b833f00f2febfa56224ce94b89
juanedcabrera <juanedcabrera@gmail.com>
2023-06-05 11:54:34 -0400

    python while loops notes

---
+++ python-while-loops.py
@@ -0,0 +1,32 @@
+# syntax - while something is boolean true, do something
+
+# x = 0
+
+# while x < 5:
+#     print(f'The current value of x is {x}')
+#     x += 1
+# else:
+#     print('X IS NOT LESS THAN 5')
+
+# # break, continue, pass
+# # break: breaks out of the current closest enclosing loop
+# # continue: goes to the top of the closest enclosing loop
+# # pass: does nothing at all
+
+# x = [1,2,3]
+# for item in x:
+#     # comment
+#     pass
+# print('end of my script')
+
+# mystring = 'Sammy'
+# for letter in mystring:
+#     if letter == 'a':
+#         continue
+#     print('This will print ', letter, 'because it continues when it hits the letter a and skips it')
+
+# mystring = 'Sammy'
+# for letter in mystring:
+#     if letter == 'a':
+#         break
+#     print('This will only print ', letter, ' because it breaks when it hits the letter a')
