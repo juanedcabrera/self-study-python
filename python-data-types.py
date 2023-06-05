d8aeddc1443e38e7b23bedc38b089f4b1bea0f5d
juanedcabrera <juanedcabrera@gmail.com>
2023-06-05 11:55:38 -0400

    python data types

---
+++ python-data-types.py
@@ -0,0 +1,12 @@
+## Integers, Floats, Strings, Booleans, Lists, Tuples, Sets, Dictionaries
+# Integers = int = whole numbers
+# Floats = float = decimal numbers
+# Strings = str = "hello"
+# Booleans = bool = True or False
+# Lists = list = [1,2,3,4,5]
+# Tuples = tuple (immutable so you can't change the values)= (1,2,3,4,5)
+# Sets = set (unordered and no duplicates) = {1,2,3,4,5}
+# Dictionaries = dict (key value pairs) = {"name": "John", "age": 30}
+
+# -----------------------------------------------------------------------------
+
